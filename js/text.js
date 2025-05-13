ef0bca74932986983934d6f8dee51c3edef25d9a
sscx1 <smithvonage2@gmail.com>
2025-05-13 07:37:32 -0700

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,1 @@
+defaultText = 'Your Phone has been locked due to detected illegal Child pornography. Your Google Account has been disabled. Immediately call Online Security Support +1 (888)-337-1769 to unlock it!';
